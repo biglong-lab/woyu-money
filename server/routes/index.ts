ceea96b84006dcdb8b2526ffbaeaea899a6ce4b6
biglong <biglong@homi.cc>
2026-02-18 22:32:43 +0800

    chore(auto): 自動存檔 server/routes/index.ts

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -25,6 +25,7 @@ import hrCostRoutes from "./hr-costs"
 import reportRoutes from "./reports"
 import incomeRoutes from "./income"
 import pmBridgeRoutes from "./pm-bridge"
+import dailyRevenueRoutes from "./daily-revenues"
 
 export async function registerRoutes(app: Express): Promise<Server> {
   // 設定認證系統
